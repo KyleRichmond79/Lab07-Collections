f78bb018e97175a0a29e02bc916ff064ea92d651
Kyle Richmond <kylerichmond79@gmail.com>
2017-09-20 21:35:32 -0700

    committing for to turn in what I have so far for credit.

---
+++ Lab07Kyle/Lab07Kyle/Inventory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07Kyle
+{
+    class Inventory<P> : IEnumerable<P> //must remember IEnumerable goes on top!!!!
+    {
+        P[] items = new P[10];
+        public int count = 0;
+
+        public void Add(P item)
+        {
+            if(count == items.Length)
+            {
+                int index = count * 2;
+                P[] extension = new P[index];
+                Array.Copy(items, extension, index);
+                items = extension;
+            }
+
+            items[count] = item;
+            count++;
+        }
+
+        public void Remove(P item)
+        {
+            int removeAt = Array.IndexOf(items, item);
+            bool remove = false;
+            P[] newArray = new P[items.Length];
+
+            for(int i = 0; i < count; i++)
+            {
+                if (remove)
+                {
+                    newArray[i - 1] = items[i];
+                }
+
+                if(i != removeAt && !remove)
+                {
+                    newArray[i] = items[i];
+                }
+
+                else
+                {
+                    remove = true;
+                }
+            }
+            items = newArray;
+            count--;
+        }
+
+        public IEnumerator<P> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        
+        }
+    }
+}
--- Lab07Kyle/Lab07Kyle/Program.cs
+++ Lab07Kyle/Lab07Kyle/Program.cs
@@ -6,7 +6,46 @@ namespace Lab07Kyle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Take a look at my new Inventory!");
+
+            Product product = new Product();
+
+            Inventory<Product> InventoryList = new Inventory<Product>();
+
+            InventoryList.Add(product);
+            InventoryList.Add(new Product { ProductMetalType = Metal.bronze });
+            InventoryList.Add(new Product { ProductMetalType = Metal.adamantine });
+            InventoryList.Add(new Product { ProductMetalType = Metal.silver });
+            InventoryList.Add(new Product { ProductMetalType = Metal.steel });
+            InventoryList.Add(new Product { ProductMetalType = Metal.cold_iron });
+            InventoryList.Add(new Product { ProductBladeLength = Blade.inches_twenty });
+            InventoryList.Add(new Product { ProductBladeLength = Blade.inches_twentfour });
+            InventoryList.Add(new Product { ProductBladeLength = Blade.inches_thirty });
+            InventoryList.Add(new Product { ProductBladeLength = Blade.inches_forty });
+
+            foreach (Product p in InventoryList)
+            {
+                Console.WriteLine(p.ProductMetalType);
+                Console.WriteLine(p.ProductBladeLength);
+                Console.WriteLine();
+            }
+
+            Console.Read();
         }
     }
 }
+
+
+/* 
+ Inventory<product> inventoryList = new Inventory<product>();
+ Product sword = new Product(
+
+
+inventoryList.Add();
+
+
+foreach(product p in inventroyList)
+{
+    Console.WriteLine(p.Product);
+}
+     */
